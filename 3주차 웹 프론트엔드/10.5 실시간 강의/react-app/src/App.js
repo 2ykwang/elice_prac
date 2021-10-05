878b430347f59926f78a00cff53fa6388048f0a1
Yeonggwang <immutable000@gmail.com>
2021-10-05 14:04:23 +0900

    edit: header를 컴포넌트화

--- 3주차 웹 프론트엔드/10.5 실시간 강의/react-app/src/App.js
+++ 3주차 웹 프론트엔드/10.5 실시간 강의/react-app/src/App.js
@@ -1,35 +1,53 @@
+import React from 'react';
 import './App.css';
 
 /*
   재사용성이 증가한다.
-  
 */
-function Navigator(){
+function Nav() {
   return (
-  <nav>
-    <ol>
-      <li><a href="1.html">html</a></li>
-      <li><a href="1.html">css</a></li>
-    </ol>
-  </nav>
-)}
+    <nav>
+      <ol>
+        <li>
+          <a href="1.html">html</a>
+        </li>
+        <li>
+          <a href="2.html">css</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
 
-function ArticleTag(){
-  return(
+function Article(props) {
+  console.log('props', props.title, props.body);
+
+  return (
     <article>
-    <h2>Welcome</h2>
-    Hello, WEB
-  </article>
-)}
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a href="index.html">{props.title}</a>
+      </h1>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <header><h1><a href='index.html'>WEB</a></h1></header>
-      <Navigator></Navigator>
-      <ArticleTag></ArticleTag>
-      <ArticleTag></ArticleTag>
-    </div>
+    <React.Fragment>
+      <Header title="title"></Header>
+      <Nav></Nav>
+      <Article title="HTML" body="HTML is ..."></Article>
+      <Article title="CSS" body="CSS is ..."></Article>
+    </React.Fragment>
   );
 }
 
