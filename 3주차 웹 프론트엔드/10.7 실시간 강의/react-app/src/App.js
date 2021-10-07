4585e6ae1fccad528c5a7f6fe52b26d32784f70d
Yeonggwang <immutable000@gmail.com>
2021-10-07 11:41:55 +0900

    edit: 네비게이션에서 선택한 글을 표시하게 함

--- 3주차 웹 프론트엔드/10.7 실시간 강의/react-app/src/App.js
+++ 3주차 웹 프론트엔드/10.7 실시간 강의/react-app/src/App.js
@@ -14,7 +14,7 @@ function Nav(props) {
   var lis = [];
   function aHandler(ev) {
     ev.preventDefault();
-    props.onSelect(ev.target.dataset.id);
+    props.onSelect(Number(ev.target.dataset.id));
   }
   for (var i = 0; i < props.src.length; i++) {
     var item = props.src[i];
@@ -51,16 +51,19 @@ function Header(props) {
 
 function App() {
   //var mode = 'READ';
-  var [mode, setMode] = useState('WELCOME');
-  
+  let [id, setId] = useState(2)
+  let [mode, setMode] = useState('WELCOME');
+
   var topics = [
     {id: 1, title: 'html', body: 'html is ..'},
     {id: 2, title: 'css', body: 'css is ..'},
   ];
+
   function selectHandler(id) {
     if (id === undefined) {
       mode = setMode('WELCOME');
-    } else {
+    } else { 
+      id = setId(id);
       mode = setMode('READ');
     }
     alert('select' + id);
@@ -68,13 +71,24 @@ function App() {
   var articleComp = <Article title="Welcome" body="Welcome is..."></Article>;
 
   if (mode === 'READ') {
-    articleComp = <Article title="READ" body="Hello, READ"></Article>;
+    let title = '';
+    let body = '';
+
+    for (let i = 0; i < topics.length; i++) {
+      const item = topics[i];
+      if (item.id === id) {
+        title = item.title;
+        body = item.body;
+        break;
+      }
+    }
+    articleComp = <Article title={title} body={body}></Article>;
   }
 
   return (
     <div>
       <Header title="html" onSelect={selectHandler}></Header>
-      <Nav src={topics} onSelect={selectHandler}></Nav> 
+      <Nav src={topics} onSelect={selectHandler}></Nav>
       {articleComp}
     </div>
   );
